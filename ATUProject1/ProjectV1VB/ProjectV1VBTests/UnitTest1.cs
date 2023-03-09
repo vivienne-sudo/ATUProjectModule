9655ef2bd28ffd31bb311abc36154c55a43138c8
Vivienne Bruen <Viviennelbruen@gmail.com>
2023-03-09 19:52:48 +0000

    adding tests and nuget packages

--- ATUProject1/ProjectV1VB/ProjectV1VBTests/UnitTest1.cs
+++ ATUProject1/ProjectV1VB/ProjectV1VBTests/UnitTest1.cs
@@ -6,6 +6,15 @@ namespace ProjectV1VBTests
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange
+            int x = 1;
+            int y = 2;
+
+            // Act
+            int result = x + y;
+
+            // Assert
+            Assert.AreEqual(3, result);
         }
     }
 }
