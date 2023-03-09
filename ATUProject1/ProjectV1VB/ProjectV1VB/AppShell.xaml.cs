75c88adea839ea35854c1af4c0ec864f8a6b9f95
Vivienne Bruen <Viviennelbruen@gmail.com>
2023-03-09 12:18:59 +0000

    Proper project solution

---
+++ ATUProject1/ProjectV1VB/ProjectV1VB/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ProjectV1VB.ViewModels;
+using ProjectV1VB.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ProjectV1VB
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ ATUProject1/ProjectV1VB/ProjectV1VB/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ProjectV1VB.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ProjectV1VB.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
