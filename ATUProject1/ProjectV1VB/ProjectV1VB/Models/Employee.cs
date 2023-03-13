0fdc4d79986342217ef557f37668b144ae238240
Vivienne Bruen <Viviennelbruen@gmail.com>
2023-03-13 16:16:30 +0000

    Create basic empty Classes

---
+++ ATUProject1/ProjectV1VB/ProjectV1VB/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectV1VB.Models
+{
+    internal class Employee
+    {
+    }
+}
---
+++ ATUProject1/ProjectV1VB/ProjectV1VB/MyDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectV1VB
+{
+    internal class MyDatabase
+    {
+    }
+}
