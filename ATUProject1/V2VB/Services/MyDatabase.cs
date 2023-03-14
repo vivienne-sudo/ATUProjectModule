fb85adb83cd641a887b4135ff5dac07797f46292
Vivienne Bruen <Viviennelbruen@gmail.com>
2023-03-14 17:26:34 +0000

    Create database

---
+++ ATUProject1/V2VB/Services/MyDatabase.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using V2VB.Model;
+
+namespace V2VB.Services
+{
+    public class MyDatabase
+    {
+        private readonly SQLiteConnection _connection;
+
+        public MyDatabase(string path)
+        {
+            _connection = new SQLiteConnection(path);
+            _connection.CreateTable<User>();
+        }
+
+        public List<User> GetUsers()
+        {
+            return _connection.Table<User>().ToList();
+        }
+
+        public User GetUser(int id)
+        {
+            return _connection.Table<User>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public void SaveUser(User user)
+        {
+            if (user.Id == 0)
+            {
+                _connection.Insert(user);
+            }
+            else
+            {
+                _connection.Update(user);
+            }
+        }
+
+        public void DeleteUser(User user)
+        {
+            _connection.Delete(user);
+        }
+    }
+}
