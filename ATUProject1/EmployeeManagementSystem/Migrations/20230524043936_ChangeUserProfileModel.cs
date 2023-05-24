f76040683c4eeb8d74326ef737095e8d52cb7781
Vivienne Bruen <Viviennelbruen@gmail.com>
2023-05-24 05:42:36 +0100

    Fix Policies and Contract view, Add annual leave days to model

---
+++ ATUProject1/EmployeeManagementSystem/Migrations/20230524043936_ChangeUserProfileModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUserProfileModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+                migrationBuilder.AddColumn<int>(
+                name: "AnnualLeaveDays",
+                table: "UserProfiles",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SickLeaveDays",
+                table: "UserProfiles",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "d203da60-e5f8-4324-9e39-69ae8f3265af", null, "Admin", "ADMIN" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnnualLeaveDays",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "SickLeaveDays",
+                table: "UserProfiles");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "dcb2abe2-d16f-4655-9746-35f65f9fc0d0", null, "Admin", "ADMIN" });
+        }
+    }
+}
